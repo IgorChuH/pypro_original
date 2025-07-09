7de84c5e2964e6766938cefee5c9826798ce757f
Чухутин Игорь <chukhutinigor@yandex.ru>
2025-07-09 22:09:10 +0300

    correct

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+def get_mask_card_number(card_number):
+    if card_number.isdigit() and len(card_number) == 16:
+        first_part = card_number[0:4]
+        second_part = card_number[4:6] + "**"
+        third_part = "*" * 4
+        last_part = card_number[12:16]
+        return f"({first_part} {second_part} {third_part} {last_part})"
+
+def get_mask_account(account_number):
+    if account_number.isdigit():
+        last_part = account_number[-4:]
+        return f'**{last_part}'
--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,11 @@
-def get_date(data_main):
+from masks import get_mask_account, get_mask_card_number
+
+def get_date(data_main: str) -> str:
     just_date = data_main[:data_main.find("T")].split('-')
     just_date.reverse()
     return '.'.join(just_date)
 
-result1 = '2024-03-11T02:26:18.671407'
-print(get_date(result1))
-
-def mask_account_card(account_card):
+def mask_account_card(account_card: str) -> str:
     name_card = []
     number_card = []
 
@@ -15,7 +14,15 @@ def mask_account_card(account_card):
             name_card.append(i)
         else:
             number_card.append(i)
+    if name_card == 'Счёт':
+        return name_card + " " + get_mask_account(number_card)
+    else:
+        return name_card + " " + get_mask_card_number(number_card)
 
+print(get_date("2024-03-11T02:26:18.671407"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+"""
     first_part = number_card[0:4]
     second_part = number_card[4:6] + ["**"]
     third_part = "*" * 4
@@ -29,8 +36,4 @@ def mask_account_card(account_card):
         ''.join(last_part)
     ])
 
-    return masked_name + ' ' + masked_number
-
-
-result = mask_account_card("Visa Platinum 7000792289606361")
-print(result)
+    return masked_name + ' ' + masked_number"""
