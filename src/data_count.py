03683b3a07492f1d6ecd41a7e596be728c1adc36
Чухутин Игорь <chukhutinigor@yandex.ru>
2025-09-06 16:45:55 +0300

    add dogstring

--- src/data_count.py
+++ src/data_count.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 
 def csv_data(path_file):
+    """Функция, принимающая путь до csv-файла, считывает его и
+        возвращающая список словарей с данными о
+        финансовых транзакциях"""
     result = []
     with open(path_file, encoding="utf-8") as file:
         reader = csv.reader(file, delimiter=";")
@@ -16,6 +19,9 @@ def csv_data(path_file):
 
 
 def pd_data(path_file):
+    """Функция, принимающая путь до csv-файла с помощью pandas, считывает его и
+            возвращающая список словарей с данными о
+            финансовых транзакциях"""
     result_read_csv = pd.read_csv(path_file, sep=";")
     return result_read_csv.head()
 
@@ -24,6 +30,9 @@ def pd_data(path_file):
 
 
 def pd_ex_data(path_file):
+    """Функция, принимающая путь до Excel-файла с помощью pandas, считывает его и
+            возвращающая список словарей с данными о
+            финансовых транзакциях"""
     result_read_csv = pd.read_excel(path_file)
     return result_read_csv.head()
 
