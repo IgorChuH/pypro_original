11323cf1d3be91ad2a5254a2f522435aa7ac35b3
Чухутин Игорь <chukhutinigor@yandex.ru>
2025-09-11 17:18:40 +0300

    д5

--- src/data_count.py
+++ src/data_count.py
@@ -1,15 +1,18 @@
 import csv
+import os
 
 import pandas as pd
 
 
-def csv_data(path_file):
+def csv_data(__file__):
     """Функция, принимающая путь до csv-файла, считывает его и
     возвращающая список словарей с данными о
     финансовых транзакциях"""
     rows = []
-    with open(path_file, encoding="utf-8", newline="") as file:
-        reader = csv.DictReader(file, delimiter=";")
+    current_dir = os.path.dirname(__file__)
+    file = os.path.join(current_dir, "..", "data", "transactions.csv")
+    with open(file, encoding="utf-8", newline="") as res_file:
+        reader = csv.DictReader(res_file, delimiter=";")
         for row in reader:
             # Если нужно — можно преобразовать типы полей здесь, например:
             # if 'amount' in row and row['amount']:
@@ -18,27 +21,31 @@ def csv_data(path_file):
     return rows
 
 
-#print(csv_data("C:\\Users\\ZIPHAI\\Decstop\\Testwork\\data\\transactions.csv"))
+# print(csv_data("C:\\Users\\ZIPHAI\\Decstop\\Testwork\\data\\transactions.csv"))
 
 
-
-def pd_data(path_file):
+def pd_data(__file__):
     """Функция принимает путь до csv-файла, считывает его и возвращает список словарей с данными о финансовых транзакциях"""
+    current_dir = os.path.dirname(__file__)
+    file = os.path.join(current_dir, "..", "data", "transactions.csv")
     # при необходимости задайте encoding, sep, parse_dates и т.д.
-    df = pd.read_csv(path_file, sep=';', encoding='utf-8')
+    df = pd.read_csv(file, sep=";", encoding="utf-8")
     # преобразуем DataFrame в список словарей
-    records = df.to_dict(orient='records')
+    records = df.to_dict(orient="records")
     return records
 
-print(pd_data("C:\\Users\\ZIPHAI\\Decstop\\Testwork\\data\\transactions.csv"))
+
+print(pd_data("transactions.csv"))
 
 
-def pd_ex_data(path_file):
+def pd_ex_data(__file__):
     """Функция, принимающая путь до Excel-файла с помощью pandas, считывает его и
     возвращающая список словарей с данными о
     финансовых транзакциях"""
-    result_read_csv = pd.read_excel(path_file)
+    current_dir = os.path.dirname(__file__)
+    file = os.path.join(current_dir, "..", "data", "transactions_excel.xlsx")
+    result_read_csv = pd.read_excel(file)
     return result_read_csv.head()
 
 
-# print(pd_ex_data("C:\\Users\\ZIPHAI\\Decstop\\Testwork\\data\\transactions_excel.xlsx"))
+print(pd_ex_data("transactions_excel.xlsx"))
